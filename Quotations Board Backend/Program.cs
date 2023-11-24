ebef4ad1d6553247bbbc4ebdb14c0fe831e9b380
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-24 12:24:10 +0300

    Add Swagger/OpenAPI documentation support

--- Quotations Board Backend/Program.cs
+++ Quotations Board Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -17,6 +19,55 @@ internal class Program
                         .AllowAnyMethod();
                 });
         });
+
+
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+        });
+
+        var securityScheme = new OpenApiSecurityScheme()
+        {
+            Description = "Enter token here as: Bearer[space] your-token",
+            Name = "Authorization",
+            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+            Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+        };
+
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+            }
+        };
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+
+            c.SwaggerDoc("v1", new() { Title = "Quatations Board", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", securityScheme);
+            c.AddSecurityRequirement(securityRequirement);
+            // Enable Annotations
+            c.EnableAnnotations();
+        });
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+
 
         // Add services to the container.
 
