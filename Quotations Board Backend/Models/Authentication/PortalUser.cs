e71ecf9f449fdb5d45737e6ae8e7f7cc1f36da03
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-24 14:49:36 +0300

    Add properties for organization details in PortalUser class

--- Quotations Board Backend/Models/Authentication/PortalUser.cs
+++ Quotations Board Backend/Models/Authentication/PortalUser.cs
@@ -2,5 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 public class PortalUser : IdentityUser
 {
+    public string OrganizationName { get; set; } = null!;
+    public string OrganizationAddress { get; set; } = null!;
+    public string AdministationEmail { get; set; } = null!;
+    public string AdministrationPhoneNumber { get; set; } = null!;
+    public string AdministationName { get; set; } = null!;
 
 }
