bdab142ca5b9917f47538724b78a8879059edc59
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-24 17:15:22 +0300

    Feat: jdgjd

---
+++ Quotations Board Backend/Migrations/20231124140901_InstitutionTypes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quotations_Board_Backend.Migrations
+{
+    public partial class InstitutionTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstitutionEmail",
+                table: "InstitutionApplications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "InstitutionTypes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstitutionTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InstitutionTypes");
+
+            migrationBuilder.DropColumn(
+                name: "InstitutionEmail",
+                table: "InstitutionApplications");
+        }
+    }
+}
