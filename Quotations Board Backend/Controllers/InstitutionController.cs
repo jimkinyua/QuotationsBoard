e5006c40e23edff1f1a5c55dadd5a4a2b64ae33e
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-27 10:18:01 +0300

    Feat: SuperAdmin Role

--- Quotations Board Backend/Controllers/InstitutionController.cs
+++ Quotations Board Backend/Controllers/InstitutionController.cs
@@ -230,7 +230,7 @@ namespace Quotations_Board_Backend.Controllers
                         }
 
                     }
-                    var institutionAdminRole = await context.Roles.FirstOrDefaultAsync(x => x.Name == CustomRoles.InstitutionAdmin);
+                    var institutionAdminRole = await context.Roles.FirstOrDefaultAsync(x => x.Name == CustomRoles.SuperAdmin);
 
                     // add user to role of InstitutionAdmin
                     var userRole = new IdentityUserRole<string>
