673184e664a8fed0e45f86cea3345c5fb500fa6b
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-28 22:01:02 +0300

    New Fileds Migration

---
+++ Quotations Board Backend/Migrations/20231128190024_NewFieldToBond.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quotations_Board_Backend.Migrations
+{
+    public partial class NewFieldToBond : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IssueNumber",
+                table: "Bonds",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IssueNumber",
+                table: "Bonds");
+        }
+    }
+}
