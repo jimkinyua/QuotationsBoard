f8b45a9a0193ef7efb3260fadcf26cf5bca0a604
James Kinyua <james.kinyua@agilebiz.co.ke>
2023-11-28 23:01:42 +0300

    Add validation attributes to NewQuotation DTO

--- Quotations Board Backend/DTOs/Quotations/NewQuotation.cs
+++ Quotations Board Backend/DTOs/Quotations/NewQuotation.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 public class NewQuotation
 {
+    [Required]
     public string BondId { get; set; } = null!;
+    [Required]
     public decimal BuyYield { get; set; }
+    [Required]
     public decimal SellYield { get; set; }
+    [Required]
     public decimal Volume { get; set; }
-    public string IssueNumber { get; set; } = null!;
+
 }
