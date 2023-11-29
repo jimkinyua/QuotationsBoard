8ef942f0b48593c238c11fdbd3cff7df6caf217e
James kinyua <james.kinyua@agilebiz.co.ke>
2023-11-29 13:25:20 +0000

    Feat: More Cols

---
+++ Quotations Board Backend/Migrations/20231129132147_MoreColumnsQuotation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quotations_Board_Backend.Migrations
+{
+    public partial class MoreColumnsQuotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Volume",
+                table: "Quotations",
+                newName: "SellVolume");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BuyVolume",
+                table: "Quotations",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyVolume",
+                table: "Quotations");
+
+            migrationBuilder.RenameColumn(
+                name: "SellVolume",
+                table: "Quotations",
+                newName: "Volume");
+        }
+    }
+}
