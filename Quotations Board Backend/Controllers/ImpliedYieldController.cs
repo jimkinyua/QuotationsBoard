615576cb36a539ab76181f6db7e9f35dba5589c4
James Kinyua <james.kinyua@agilebiz.co.ke>
2024-01-18 15:18:36 +0300

    Refactor variable name in ImpliedYieldController.cs

--- Quotations Board Backend/Controllers/ImpliedYieldController.cs
+++ Quotations Board Backend/Controllers/ImpliedYieldController.cs
@@ -256,7 +256,7 @@ namespace Quotations_Board_Backend.Controllers
             {
                 using (var db = new QuotationsBoardContext())
                 {
-                    var DateInQuestion_ = DateTime.Now.Date;
+                    var DateInQuestion = DateTime.Now.Date;
                     // var DateInQuestion = DateInQuestion_.AddDays(-1);
                     var LastWeek = DateInQuestion.AddDays(-7);
 
