b5c2ec9f34ab7c8f872318afe9cd7c4a68255c1e
James Kinyua <james.kinyua@agilebiz.co.ke>
2024-02-06 15:51:18 +0300

    Update Quotation class with default values for BuyVolume and SellVolume

--- Quotations Board Backend/Models/Quotations/Quotation.cs
+++ Quotations Board Backend/Models/Quotations/Quotation.cs
@@ -8,8 +8,8 @@ public class Quotation
     public string Id { get; set; } = null!;
     public decimal BuyingYield { get; set; }
     public decimal SellingYield { get; set; }
-    public decimal BuyVolume { get; set; }
-    public decimal SellVolume { get; set; }
+    public decimal BuyVolume { get; set; } = 50000000;
+    public decimal SellVolume { get; set; } = 50000000;
     [ForeignKey("Institution")]
     public string InstitutionId { get; set; } = null!;
     public Institution Institution { get; set; } = null!;
