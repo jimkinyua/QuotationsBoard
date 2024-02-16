307ecca892678edbd6ce91286e41139c695786b5
James Kinyua <james.kinyua@agilebiz.co.ke>
2024-02-16 12:50:30 +0300

    Feat: Change To Double

---
+++ Quotations Board Backend/Migrations/20240216094453_RYER.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quotations_Board_Backend.Migrations
+{
+    public partial class RYER : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Tenor",
+                table: "TBills",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Tenor",
+                table: "TBills",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
